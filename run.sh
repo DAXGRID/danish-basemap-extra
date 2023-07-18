df849df841fffabf727b0ab82dad30fa7435b075
Rune Nielsen <runenielsen@runbox.com>
2023-07-18 13:58:22 +0200

    make logging for extracting properties more explicit

--- run.sh
+++ run.sh
@@ -15,37 +15,37 @@ log_time_text "Starting download files."
 ./DanishGeoJsonExtractor "$(pwd)/appsettings.json"
 
 # Handle Bygning
-log_time_text "Processing bygning."
+log_time_text "Extracting only necessary properties from bygning."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'bygning' as objecttype, status FROM bygning" $output_dir/danish-basemap.geojson $output_dir/bygning.geojson
 
 # Handle Skel
-log_time_text "Processing skel."
+log_time_text "Extracting only necessary properties from skel."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'skel' as objecttype FROM matrikelskel" $output_dir/danish-basemap.geojson $output_dir/matrikelskel.geojson
 
 # Vejkant
-log_time_text "Processing vejkant."
+log_time_text "Extracting only necessary properties from vejkant."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'vejkant' as objecttype FROM vejkant" $output_dir/danish-basemap.geojson $output_dir/vejkant.geojson
 
 # Vejmidte
-log_time_text "Processing vejmidte."
+log_time_text "Extracting only necessary properties from vejmidte."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'vejmidte' as objecttype FROM vejmidte" $output_dir/danish-basemap.geojson $output_dir/vejmidte.geojson
 
 # Helle
-log_time_text "Processing helle."
+log_time_text "Extracting only necessary properties from helle."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'helle' as objecttype FROM helle" $output_dir/danish-basemap.geojson $output_dir/helle.geojson
 
 # Nedloebsrist
-log_time_text "Processing nedloebsrist."
+log_time_text "Extracting only necessary properties from nedloebsrist."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'nedloebsrist' as objecttype FROM nedloebsrist" $output_dir/danish-basemap.geojson $output_dir/nedloebsrist.geojson
 
 # Broenddaeksel
-log_time_text "Processing broenddaeksel."
+log_time_text "Extracting only necessary properties from broenddaeksel."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'broenddaeksel' as objecttype FROM broenddaeksel" $output_dir/danish-basemap.geojson $output_dir/broenddaeksel.geojson
 
 # Lysmast
-log_time_text "Processing mast."
+log_time_text "Extracting only necessary properties from mast."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'mast' as objecttype FROM mast" $output_dir/danish-basemap.geojson $output_dir/mast.geojson
 
 # Hegn
-log_time_text "Processing hegn."
+log_time_text "Extracting only necessary properties from hegn."
 ogr2ogr -f GeoJSONSeq -append -sql "SELECT 'hegn' as objecttype FROM hegn" $output_dir/danish-basemap.geojson $output_dir/hegn.geojson
