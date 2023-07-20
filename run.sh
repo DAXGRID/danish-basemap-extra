06c036e91d51375993686ac4f002f63ff38bfec0
Rune Nielsen <runenielsen@runbox.com>
2023-07-20 19:26:33 +0200

    fix issue not being able to upload file

--- run.sh
+++ run.sh
@@ -82,4 +82,5 @@ log_time_text 'Building tileset from danish-basemap.geojson.'
 tippecanoe --minimum-zoom=16 --maximum-zoom=16 --force --output=$output_dir/objects.mbtiles $output_dir/danish-basemap.geojson
 
 # Upload file to the filesever
-curl -u $FILE_SERVER_USERNAME:$FILE_SERVER_PASSWORD -F "file=@$(output_dir)/objects.mbtiles" "$FILE_SERVER_URI/?upload"
+log_time_text 'Uploading the tiles to the fileserver.'
+curl -u $FILE_SERVER_USERNAME:$FILE_SERVER_PASSWORD -F "file=@$output_dir/objects.mbtiles" "$FILE_SERVER_URI/?upload"
